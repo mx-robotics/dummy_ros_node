4cc8fc8e612641cea47168a2452e527da79da6c4
Markus Bader <markus.bader@tuwien.ac.at>
2020-06-23 20:25:42 +0200

    update

--- include/template_package/template_package_ros.h
+++ include/template_package/template_package_ros.h
@@ -22,11 +22,11 @@ private:
     dynamic_reconfigure::Server<template_package::TemplatePackageConfig> reconfigureServer_; /// parameter server stuff
     dynamic_reconfigure::Server<template_package::TemplatePackageConfig>::CallbackType reconfigureFnc_;  /// parameter server stuff
     void callbackConfigTemplatePackage ( template_package::TemplatePackageConfig &config, uint32_t level ); /// callback function on incoming parameter changes
+    void callbackLaser ( const sensor_msgs::LaserScan::ConstPtr& msg);
+    void callbackTimerWatchdog(const ros::TimerEvent& event);
 
 public:
     void init(ros::NodeHandle &public_nh, ros::NodeHandle &private_nh);
-    void callbackLaser ( const sensor_msgs::LaserScan::ConstPtr& msg);
-    void callbackTimerWatchdog(const ros::TimerEvent& event);
 
 };
 }
